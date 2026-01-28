82982ebbecf059609b21fa8113359fcec5188b8f
StanciuDragosIoan <XDRST0501@dsb.dk>
2026-01-28 17:54:56 +0200

    test

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import Groq from 'groq-sdk';
 import readline from 'readline';
 import 'dotenv/config';
+import express from 'express';
 // const groq = new Groq({
 //   apiKey: process.env.GROQ_API_KEY,
 // });
+
+const app = express();
 
 const groq = new Groq({
   apiKey: process.env.GROQ_API_KEY,
@@ -26,28 +29,36 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-async function chat(userInput) {
-  memory.push({ role: 'user', content: userInput });
+app.use(express.json());
+app.use('/brainy', express.static('public/brainy'));
 
-  const response = await groq.chat.completions.create({
-    model: 'llama-3.1-8b-instant',
-    messages: memory,
-  });
 
-  const reply = response.choices[0].message.content;
-
-  memory.push({ role: 'assistant', content: reply });
-
-  console.log('\n🤖:', reply, '\n');
-}
-
-console.log("AI Agent started. Type 'exit' to quit.\n");
-
-rl.on('line', async (input) => {
-  if (input.toLowerCase() === 'exit') {
-    rl.close();
-    return;
-  }
-
-  await chat(input);
+app.listen(port, () => {
+  console.log(`🤖 AI-chan running at http://localhost:${port}`);
 });
+
+// async function chat(userInput) {
+//   memory.push({ role: 'user', content: userInput });
+
+//   const response = await groq.chat.completions.create({
+//     model: 'llama-3.1-8b-instant',
+//     messages: memory,
+//   });
+
+//   const reply = response.choices[0].message.content;
+
+//   memory.push({ role: 'assistant', content: reply });
+
+//   console.log('\n🤖:', reply, '\n');
+// }
+
+// console.log("AI Agent started. Type 'exit' to quit.\n");
+
+// rl.on('line', async (input) => {
+//   if (input.toLowerCase() === 'exit') {
+//     rl.close();
+//     return;
+//   }
+
+//   await chat(input);
+// });
